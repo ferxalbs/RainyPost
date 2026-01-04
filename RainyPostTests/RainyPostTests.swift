20a69fe3226a419a2284e1e4f1d92fffa8019a40
ferxalbs <enosislabsoff@gmail.com>
2026-01-04 18:17:52 -0500

    Initial Commit

---
+++ RainyPostTests/RainyPostTests.swift
@@ -0,0 +1,16 @@
+//
+//  RainyPostTests.swift
+//  RainyPostTests
+//
+//  Created by Fer on 1/4/26.
+//
+
+import Testing
+
+struct RainyPostTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
